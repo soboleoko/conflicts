99d7b8f8f3497f643d70985ee3029989b9249291
Marcin Sobolewski <mlodysobol@gmail.com>
2024-11-13 09:53:13 +0100

    Data

--- conflict/src/conflicts/c2/c2.java
+++ conflict/src/conflicts/c2/c2.java
@@ -1,4 +1,8 @@
 package conflicts.c2;
 
 public class c2 {
+    int a = 2;
+    String test = "test";
+    String firstName = "Marcin";
+    String lastName = "Sobolewski";
 }
--- conflict/src/conflicts/c3/c3.java
+++ conflict/src/conflicts/c3/c3.java
@@ -1,4 +1,8 @@
 package conflicts.c3;
 
 public class c3 {
+    int a = 3;
+    String test = "test";
+    String firstName = "Marcin";
+    String lastName = "Sobolewski";
 }
